ee55d52442f5fff14db2f78a763e489c2aa03b4b
softrobstp <softwarerobotics@gmail.com>
2013-07-12 22:33:51 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+
+var fs = require('fs');
+var buf=new Buffer(fs.readFileSync('index.html'));
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString("utf-8",0));
 });
 
 var port = process.env.PORT || 5000;
