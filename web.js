20b8f521fcd947aeed7988d7403efcde57de3e8a
Ubuntu <ubuntu@ip-172-31-44-216.us-west-2.compute.internal>
2013-07-18 14:56:09 +0000

    Modify web.js as in prof's instructions.

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
-
-var fs = require('fs');
-var buf=new Buffer(fs.readFileSync('index.html'));
-
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8",0));
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
